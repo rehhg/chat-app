6d9468c52e7731acfecba4cdb86fe2bb5b20a906
Serhiy Pihura <Serhiy_Pihura@epam.com>
2019-11-12 11:34:43 +0200

    Init ChatConsumer

---
+++ src/chat/consumers.py
@@ -0,0 +1,22 @@
+import json
+
+from channels.generic.websocket import WebsocketConsumer
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data=None, bytes_data=None):
+        if not text_data:
+            raise Exception('Text data is None. WebSocket Consumer can\'t continue consuming process')
+
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
