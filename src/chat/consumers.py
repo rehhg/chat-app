f47b15ce7831cc56fa993404a49c06d2c29137b5
Serhiy Pihura <Serhiy_Pihura@epam.com>
2019-11-13 13:15:03 +0200

    Implement saving and loading messages

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -1,43 +1,98 @@
 import json
 
-from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+
+from django.contrib.auth import get_user_model
+
+from .models import Message
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+User = get_user_model()
+
+
+class ChatConsumer(WebsocketConsumer):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.room_name = None
         self.room_group_name = None
 
-    async def connect(self):
+    def fetch_messages(self, data):
+        """
+        Fetch messages from db
+        :param data:
+        :return:
+        """
+        messages = Message.last_10_messages()
+        self.send_message({
+            'command': 'messages',
+            'messages': self.messages_to_json(messages)
+        })
+
+    def new_message(self, data):
+        """
+        Create new message in the DB and send it to chat
+        :param data:
+        :return:
+        """
+        author_user = User.objects.filter(username=data['from'])[0]
+        message = Message.objects.create(author=author_user, content=data['message'])
+
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+
+        return result
+
+    def message_to_json(self, message):
+        return {
+            'id': message.id,
+            'author': message.author.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
+
+    commands = {
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    async def receive(self, text_data=None, bytes_data=None):
+    def receive(self, text_data=None, bytes_data=None):
+        # TODO: investigate if we need this
         if not text_data:
             raise Exception('Text data is None. WebSocket Consumer can\'t continue consuming process')
 
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
 
-        # Send message to room group
-        await self.channel_layer.group_send(
+        self.commands[data['command']](self, data)
+
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -45,11 +100,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+    def chat_message(self, event):
+        self.send(text_data=json.dumps(event['message']))
--- src/chat/views.py
+++ src/chat/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 
@@ -8,7 +9,9 @@ def index(request):
     return render(request, 'chat/index.html', {})
 
 
+@login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username))
     })
